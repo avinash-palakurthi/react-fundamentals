c8135a694c186cdfec84101e3b5c4bce648d4d90
Avinash <palakurthiavinash07@gmail.com>
2023-07-18 12:40:33 +0530

    jsx started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 const App = () => {
-  return (
-    <>
-      <h1>JSX</h1>
-    </>
+  const jsx = (
+    <div className="divId">
+      <h1 className="headerId">hello jsx</h1>
+      <h2>{String(true)} </h2>
+      <h3>{String(undefined)} </h3>
+      <h4>{null} </h4>
+    </div>
   );
+  console.log(jsx);
+  return jsx;
 };
 
 export default App;
