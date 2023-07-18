e038edd7ae0f8ef5fbb796d83f023c3700199ee5
Avinash <palakurthiavinash07@gmail.com>
2023-07-18 17:33:11 +0530

    movie project

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,10 @@
-const App = () => {
-  const pets = [
-    {
-      name: "Dolly",
-      type: "cat",
-      description: "Dolly is funny and loves to be around people",
-      skills: ["fetch", "play dead", "roll over"],
-      image:
-        "https://images.unsplash.com/photo-1583524505974-6facd53f4597?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80",
-    },
-    {
-      name: "Molly",
-      type: "cat",
-      description: "Molly is a sweet and cuddly cat stretch a lot!",
-      skills: ["play dead", "meow", "purr"],
-      image:
-        "https://images.unsplash.com/photo-1570824104453-508955ab713e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=711&q=80",
-    },
-    {
-      name: "Molly1",
-      type: "cat",
-      description: "Molly is a sweet and cuddly cat stretch a lot!",
-      skills: ["play dead", "meow", "purr"],
-      image:
-        "https://images.unsplash.com/photo-1570018144715-43110363d70a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=731&q=80",
-    },
-  ];
+import MovieComponent from "./MovieComponent";
 
+const App = () => {
   return (
-    <div className="pets">
-      <h1 className="heading">My Pets</h1>
-      <ul className="container">
-        {pets.map((pet, index) => (
-          <li key={index}>
-            <h1>{pet.name}</h1>
-            <p>{pet.description}</p>
-            <img
-              src={pet.image}
-              alt={pet.name}
-              height={"200px"}
-              width={"250px"}
-            />
-            <ul className="skills">
-              {pet.skills.map((skill, index) => (
-                <li key={skill}>{skill}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>Components , Props & State</h1>
+      <MovieComponent />
     </div>
   );
 };
---
+++ src/data/movieData.jsx
@@ -0,0 +1,82 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Salaar: Part 1 – Ceasefire",
+    year: 2023,
+    director: "Prashanth Neel",
+    rating: "R",
+    runtime: 136,
+    poster: "https://pbs.twimg.com/media/F0PgoC3WwAE_kHd.jpg:large",
+  },
+  {
+    id: 2,
+    title: "RRR",
+    year: 2022,
+    director: "S. S. Rajamouli",
+    rating: "PG-13",
+    runtime: 178,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg",
+  },
+  {
+    id: 3,
+    title: "KGF",
+    year: 2022,
+    director: "Prashanth Neel",
+    rating: "PG-13",
+    runtime: 152,
+    poster:
+      "https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2022/03/05/kgf.png",
+  },
+  {
+    id: 4,
+    title: "The Last Kingdom: Seven Kings Must Die",
+    year: 2023,
+    director: "Edward Bazalgette",
+    rating: "PG-13",
+    runtime: 148,
+    poster:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ17XGATi5V_xrlBKORLKAc6kVh-Kejj6CwnbknLkVxO3N_Gb3m",
+  },
+  {
+    id: 5,
+    title: "Beast",
+    year: 2022,
+    director: "Nelson Dilipkumar",
+    rating: "PG-13",
+    runtime: 169,
+    poster:
+      "https://igimages.gumlet.io/tamil/home/beast260322_1.jpg?w=376&dpr=2.6",
+  },
+  {
+    id: 6,
+    title: "Operation Fortune: Ruse de Guerre",
+    year: 2012,
+    director: "Guy Ritchie",
+    rating: "PG-13",
+    runtime: 164,
+    poster:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTnSuXyUtRNvTVgRqecf6adYdrkGHmFxe-_84RXazjtCvTLd1UI",
+  },
+  {
+    id: 7,
+    title: "Mission: Impossible – Dead Reckoning Part One",
+    year: 2023,
+    director: "Christopher McQuarrie",
+    rating: "PG-13",
+    runtime: 178,
+    poster:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRPunAXQI8_X7p1MYVmiRF-hSOinkCiCqca8p9_0vB22qS2ttjx",
+  },
+
+  {
+    id: 8,
+    title: "John Wick ",
+    year: 2021,
+    director: " Chad Stahelski",
+    rating: "PG-13",
+    runtime: 178,
+    poster:
+      "https://m.media-amazon.com/images/I/81F5PF9oHhL._AC_UF894,1000_QL80_.jpg",
+  },
+];
